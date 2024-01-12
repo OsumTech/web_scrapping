1ce9c0d8d47064177e264b025791e985ce83a458
MuhmammadAhmed <101988315+OsumTech@users.noreply.github.com>
2024-01-12 20:43:13 +0500

    Update operators.py

--- operators.py
+++ operators.py
@@ -5,11 +5,15 @@ print(x + y)
 print(x - y)
 print(x * y)
 print(x / y)
+
 Comparison Operators
 x < y
 x > y
 x != y
 x == y
+
 Logical Operators
 x > 2 and y < 8
 x > 2 and y <= 8
+
+x > 2 or y < 8
